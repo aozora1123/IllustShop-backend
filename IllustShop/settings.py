816c43d6fd4db753e77309f61d7a43a84150c4df
Kaeluarga <aozora1123@gmail.com>
2023-05-15 18:51:23 +0800

    新增部署於Render所需文件，並變更資料庫連線方式

--- IllustShop/settings.py
+++ IllustShop/settings.py
@@ -13,6 +13,7 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 from pathlib import Path
 from dotenv import load_dotenv
 import os
+import dj_database_url
 load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -81,14 +82,15 @@ WSGI_APPLICATION = 'IllustShop.wsgi.application'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': os.getenv("DB_NAME"),
-        'USER': os.getenv("DB_USER"),
-        'PASSWORD': os.getenv("DB_PASSWORD"),
-        'HOST': os.getenv("DB_HOST"),
-        'PORT': os.getenv("DB_PORT")
-    }
+    #'default': {
+    #    'ENGINE': 'django.db.backends.postgresql',
+    #    'NAME': os.getenv("DB_NAME"),
+    #    'USER': os.getenv("DB_USER"),
+    #    'PASSWORD': os.getenv("DB_PASSWORD"),
+    #    'HOST': os.getenv("DB_HOST"),
+    #    'PORT': os.getenv("DB_PORT")
+    #}
+    'default': dj_database_url.config(os.getenv('DATABASE_URL'), conn_max_age=600),
 }
 
 
---
+++ build.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+set -o errexit  # exit on error
+
+pip install --upgrade pip
+pip install -r requirements.txt
+
+python manage.py collectstatic --no-input
+python manage.py migrate
